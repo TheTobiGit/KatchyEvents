3bb9f47022e3d57fb5a59e86e3da4a3e29dc2f1a
Gideon Sarfo <aemonsarfo@outlook.com>
2022-07-27 17:21:11 +0000

    reverse

--- loader.js
+++ loader.js
@@ -1,25 +1,25 @@
-document.onreadystatechange = function() {
-            if (document.readyState !== "complete") {
-                document.querySelector(
-                  "body").style.visibility = "hidden";
-                document.querySelector(
-                  "#spinner").style.visibility = "visible";
-            } else {
-                document.querySelector(
-                  "#spinner").style.display = "none";
-                document.querySelector(
-                  "body").style.visibility = "visible";
-            }
-        };
+// document.onreadystatechange = function() {
+//             if (document.readyState !== "complete") {
+//                 document.querySelector(
+//                   "body").style.visibility = "hidden";
+//                 document.querySelector(
+//                   "#spinner").style.visibility = "visible";
+//             } else {
+//                 document.querySelector(
+//                   "#spinner").style.display = "none";
+//                 document.querySelector(
+//                   "body").style.visibility = "visible";
+//             }
+//         };
 
-// $( ".chaotic-orbit" ).load( "test.html", function() { 
-//   $(".chaotic-orbit").fadeOut("slow"); 
+// (".chaotic-orbit" ).load( "index.html", function() { 
+//   (".chaotic-orbit").fadeOut("slow"); 
 // });
 
-// if (document.readyState == 'complete') {
-//     $(".chaotic-orbit").fadeOut("slow");
-// } else {
-//     $(window).load(function () {
-//         $(".chaotic-orbit").fadeOut("slow");
-//     })
-// };
+if (document.readyState == 'complete') {
+    $(".chaotic-orbit").fadeOut("slow");
+} else {
+    $(window).load(function () {
+        $(".chaotic-orbit").fadeOut("slow");
+    })
+};
